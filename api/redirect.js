c229f63695931619f8a298725207866ffc801ac0
Siddesh Sankhya <79219618+Siddesh7@users.noreply.github.com>
2024-01-25 12:42:17 +0530

    Create redirect.js

---
+++ api/redirect.js
@@ -0,0 +1,16 @@
+// api/redirect.js
+
+const { send } = require('micro');
+
+module.exports = async (req, res) => {
+  const { query } = req;
+  const subdomain = query.subdomain;
+
+  if (subdomain) {
+    const linkedinProfile = 'https://www.linkedin.com/in/siddesh.eth';
+    const redirectUrl = `${linkedinProfile}/${subdomain}`;
+    return send(res, 302, null, { Location: redirectUrl });
+  }
+
+  return send(res, 400, 'Bad Request');
+};
